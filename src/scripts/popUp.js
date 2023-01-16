8eb35bc5838006094d0b603c34e469c849498368
danil <danil.polovinkin.1994@gmail.com>
2023-01-16 18:09:03 +0300

    added new popUp page,added some logic to it

---
+++ src/scripts/popUp.js
@@ -0,0 +1,22 @@
+const accrodionButtons = document.querySelectorAll(".pop-up__answer_plus");
+const questionBlock = document.querySelectorAll(".pop-up__answer_question-top")
+
+accrodionButtons.forEach(item => {
+	item.addEventListener("click" , () => {
+		if(item.hasAttribute("Data")){
+			item.setAttribute("src" , "img/PopUp/minusIcon.svg")
+			item.removeAttribute("Data")
+		}else {
+			item.setAttribute("src" , "img/PopUp/plusIcon.svg")
+			item.setAttribute("Data","" )
+		}
+		item.parentNode.classList.toggle("active")
+
+		const concreteMain = item.parentNode.parentNode
+		concreteMain.classList.toggle("active")
+
+		const textToToggle = concreteMain.querySelector(".pop-up__answer__text_text")
+		textToToggle.classList.toggle("active")
+		console.log(textToToggle)
+	})
+})
